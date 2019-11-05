c4f8d5f997d3d168bfa99320f67cfb7967a50cd4
Emily Kothe <emily.kothe@deakin.edu.au>
2019-11-06 09:27:22 +1100

    Making sure everything I have is on git

---
+++ fed-plots-2.R
@@ -0,0 +1,93 @@
+library(ggridges)
+library(ggplot2)
+library(dplyr)
+library(papaja)
+library(cowplot)
+
+ggplot(climate, aes(y = condition, x = cc_int, colour = condition, fill = condition)) + 
+  geom_density_ridges(
+    alpha = 1, scale = 3
+  ) + 
+  scale_colour_brewer(type = "qual", palette = 2, direction = 1,
+                      aesthetics = c("colour", "fill"))  +
+  labs(x = "Intention", y = "Message") + 
+  theme(legend.position = "none")
+
+
+ggplot(climate, aes(condition, cc_int, colour = condition, fill = condition)) + 
+  geom_flat_violin(position = position_nudge(x = .2, y = 0), adjust = 2)+
+  geom_point(position = position_jitter(width = .10), size = .25) + 
+  geom_boxplot(outlier.shape = NA, alpha = 0.5, width = .1, colour = "BLACK") +
+  theme_classic() + coord_flip() + labs(x = "", y = "Intention") + theme(legend.position = "none")
+
+sevplot <- ggplot(filter(climate, condition =="Control" | condition == "Severity"), aes(y = condition, x = cc_sev, fill = condition)) +  
+  geom_density_ridges(scale = 4) + theme_apa() + labs(y = "Message", x = "Severity Score") + theme(legend.position = "none")
+
+ggplot(climate, aes(condition, cc_int, colour = condition, fill = condition)) + 
+  scale_fill_manual(values = c("grey", "tan2", "darkgreen", "cornflowerblue", "darkblue", "purple", "pink")) +
+  scale_colour_manual(values = c("grey", "tan2", "darkgreen", "cornflowerblue", "darkblue", "purple", "pink")) +
+  #geom_flat_violin(position = position_nudge(x = .2, y = 0), adjust = 2)+
+  geom_point(position = position_jitter(width = .10), size = .25) + 
+  geom_boxplot(position = position_nudge(x = .2, y = 0), outlier.shape = NA, alpha = 0.5, width = .1, colour = "BLACK") +
+  theme_classic() + coord_flip() + labs(x = "", y = "Intention") + theme(legend.position = "none")
+
+
+sevplot <- ggplot(filter(climate, condition =="Control" | condition == "Severity"), aes(condition, cc_int, colour = condition, fill = condition)) + 
+  scale_fill_manual(values = c("grey", "pink")) +
+  scale_colour_manual(values = c("grey", "pink")) +
+  #geom_flat_violin(position = position_nudge(x = .2, y = 0), adjust = 2)+
+  geom_point(position = position_jitter(width = .10), size = .25) + 
+  geom_boxplot(position = position_nudge(x = -0.2, y = 0), outlier.shape = NA, alpha = 0.5, width = .1, colour = "BLACK") +
+  theme_classic() +  labs(x = "", y = "Intention") + theme(legend.position = "none") +
+  labs(x = "", y = "")
+
+susplot <- ggplot(filter(climate, condition =="Control" | condition == "Susceptibility"), aes(condition, cc_int, colour = condition, fill = condition)) + 
+  #geom_flat_violin(position = position_nudge(x = .2, y = 0), adjust = 2)+
+  scale_fill_manual(values = c("grey", "purple")) +
+  scale_colour_manual(values = c("grey", "purple")) +
+  geom_point(position = position_jitter(width = .10), size = .25) + 
+  geom_boxplot(position = position_nudge(x = -0.2, y = 0), outlier.shape = NA, alpha = 0.5, width = .1, colour = "BLACK") +
+  theme_classic() +  labs(x = "", y = "Intention") + theme(legend.position = "none") + 
+  labs(x = "", y = "")
+
+mrrplot <- ggplot(filter(climate, condition =="Control" | condition == "MRR"), aes(condition, cc_int, colour = condition, fill = condition)) + 
+  #geom_flat_violin(position = position_nudge(x = .2, y = 0), adjust = 2)+
+  scale_fill_manual(values = c("grey", "darkblue")) +
+  scale_colour_manual(values = c("grey", "darkblue")) +
+  geom_point(position = position_jitter(width = .10), size = .25) + 
+  geom_boxplot(position = position_nudge(x = -0.2, y = 0), outlier.shape = NA, alpha = 0.5, width = .1, colour = "BLACK") +
+  theme_classic() + labs(x = "", y = "Intention") + theme(legend.position = "none") + 
+  labs(x = "", y = "")
+
+
+seplot <- ggplot(filter(climate, condition =="Control" | condition == "Self-Efficacy"), aes(condition, cc_int, colour = condition, fill = condition)) + 
+  #geom_flat_violin(position = position_nudge(x = .2, y = 0), adjust = 2)+
+  scale_fill_manual(values = c("grey", "cornflowerblue")) +
+  scale_colour_manual(values = c("grey", "cornflowerblue")) +
+  geom_point(position = position_jitter(width = .10), size = .25) + 
+  geom_boxplot(position = position_nudge(x = -0.2, y = 0), outlier.shape = NA, alpha = 0.5, width = .1, colour = "BLACK") +
+  theme_classic() + labs(x = "", y = "Intention") + theme(legend.position = "none") + 
+  labs(x = "", y = "")
+
+
+replot <- ggplot(filter(climate, condition =="Control" | condition == "Response Efficacy"), aes(condition, cc_int, colour = condition, fill = condition)) + 
+  #geom_flat_violin(position = position_nudge(x = .2, y = 0), adjust = 2)+
+  scale_fill_manual(values = c("grey", "darkgreen")) +
+  scale_colour_manual(values = c("grey", "darkgreen")) +
+  geom_point(position = position_jitter(width = .10), size = .25) + 
+  geom_boxplot(position = position_nudge(x = -0.2, y = 0), outlier.shape = NA, alpha = 0.5, width = .1, colour = "BLACK") +
+  theme_classic() +   labs(x = "", y = "") + theme(legend.position = "none") 
+
+
+
+rcplot <- ggplot(filter(climate, condition =="Control" | condition == "Response Costs"), aes(condition, cc_int, colour = condition, fill = condition)) + 
+  #geom_flat_violin(position = position_nudge(x = .2, y = 0), adjust = 2)+
+  scale_fill_manual(values = c("grey", "tan2")) +
+  scale_colour_manual(values = c("grey", "tan2")) +
+  geom_point(position = position_jitter(width = .10), size = .25) + 
+  geom_boxplot(position = position_nudge(x = -0.2, y = 0), outlier.shape = NA, alpha = 0.5, width = .1, colour = "BLACK") +
+  theme_classic() +   labs(x = "", y  = "") + theme(legend.position = "none")
+
+
+plot_grid(sevplot, susplot, mrrplot, seplot, replot, rcplot, ncol=3, nrow = 2, labels = c("A", "B", "C","D", "E", "F"))
+
